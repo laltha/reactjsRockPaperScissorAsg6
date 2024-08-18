daec7564c40db3a956a852e251b12f96261a9d26
lalitha <lalithalikes@gmail.com>
2024-08-18 07:13:58 +0000

    rock

--- src/GameResultView/styledComponents.js
+++ src/GameResultView/styledComponents.js
@@ -1,27 +1,26 @@
 import styled from 'styled-components'
 
 export const ShowResultContainer = styled.div`
-    width:100%;
+  width: 100%;
 `
 
 export const ResultImagesContainer = styled.div`
-    display:flex;
-    flex-direction:row;
-    justify-content:space-around;
-
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 `
 
 export const ResultTextImgContainer = styled.div``
 
 export const ResultText = styled.p`
-    text-align:center;
-    font-weight:bold;
-    font-size:20px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
 `
 
 export const ResultImageItem = styled.img`
-    width:150px;
-    height:150px;
+  width: 150px;
+  height: 150px;
 `
 
 export const ResultButtonContainer = styled.div`
@@ -32,13 +31,13 @@ export const ResultButtonContainer = styled.div`
 `
 
 export const PlayAgainButton = styled.button`
-    border:0;
-    background-color:#ffffff;
-    color:#000000;
-    font-family:'Roboto';
-    font-size:15px;
-    padding:10px;
-    font-weight:bold;
-    border-radius:10px;
-    cursor:pointer;
+  border: 0;
+  background-color: #ffffff;
+  color: #000000;
+  font-family: 'Roboto';
+  font-size: 15px;
+  padding: 10px;
+  font-weight: bold;
+  border-radius: 10px;
+  cursor: pointer;
 `
